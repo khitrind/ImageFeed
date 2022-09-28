62b95257a0104e984bb0e7bdafd723f41e2a63a7
Dmitry Khitrin <DmitryKhitrin@gmail.com>
2022-09-28 18:21:11 +0300

    Sprint-01: Added tableView and mock data

---
+++ ImageFeed/ImageList/ImagesListCell.swift
@@ -0,0 +1,16 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Dmitry Khitrin on 28/09/2022.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+	static let reuseIdentifier = "ImagesListCell"
+	@IBOutlet weak var dateLabel: UILabel!
+	@IBOutlet weak var cellImage: UIImageView!
+	@IBOutlet weak var likeButton: UIButton!
+
+}
