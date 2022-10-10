c2c8f98aeb33715a7c5c225968747a57c427df7d
Dmitry Khitrin <DmitryKhitrin@gmail.com>
2022-10-10 15:11:27 +0300

    Sprint-09: Added SingleImageViewController

---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Dmitry Khitrin on 10/10/2022.
+//
+
+import UIKit
+
+class SingleImageViewController: UIViewController {
+
+	@IBOutlet weak private var singleImage: UIImageView!
+
+
+}
