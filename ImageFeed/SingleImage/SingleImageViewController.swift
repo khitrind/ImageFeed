84f245bc31a012c3a5d9dafa13ac9cb788f27516
Dmitry Khitrin <DmitryKhitrin@gmail.com>
2022-10-11 15:51:58 +0300

    Sprint-09: Scroll view

--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -15,16 +15,60 @@ class SingleImageViewController: UIViewController {
 		}
 	}
 
+	@IBOutlet weak var singleImage: UIImageView!
+	@IBOutlet weak var scrollView: UIScrollView!
+
 	@IBAction func didTapBackButton() {
 		dismiss(animated: true)
 	}
 
-	@IBOutlet weak var singleImage: UIImageView!
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		singleImage.image = image
+	@IBAction func didClickShareButtom() {
+		let share = UIActivityViewController(
+			activityItems: [singleImage],
+			applicationActivities: nil
+		)
+		present(share, animated: true, completion: nil)
 	}
 
 
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		setImage()
+		configureScrollViewScale()
+	}
+
+	private func setImage() {
+		singleImage.image = image
+		rescaleScrollViewForPerfectView(image: image)
+	}
+
+	private func configureScrollViewScale() {
+		scrollView.minimumZoomScale = 0.1
+		scrollView.maximumZoomScale = 1.25
+	}
+
+	private func rescaleScrollViewForPerfectView(image: UIImage) {
+		let containerSize = view.bounds.size
+		let imageSize = image.size
+		let hScale = containerSize.width / imageSize.width
+		let vScale = containerSize.height / imageSize.height
+		let scale = max(hScale, vScale)
+		let expectedContentSize = CGSize(
+			width: imageSize.width * scale,
+			height: imageSize.height * scale
+		)
+		UIView.animate(withDuration: 0.25, delay: 0, options: .curveEaseInOut) {
+			self.scrollView.setZoomScale(scale, animated: false)
+			let x = (expectedContentSize.width - containerSize.width) / 2
+			let y = (expectedContentSize.height - containerSize.height) / 2
+			self.scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+		} completion: { _ in
+		}
+	}
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+	func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+		singleImage
+	}
 }
