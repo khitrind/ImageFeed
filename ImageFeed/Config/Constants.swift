40dab9f853061c0c4302c7478116a95f03120ed5
Dmitry Khitrin <DmitryKhitrin@gmail.com>
2022-11-13 16:52:23 +0200

    Sprint-10: Added unsplash keys

---
+++ ImageFeed/Config/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Dmitry Khitrin on 13/11/2022.
+//
+
+import Foundation
+
+let AccessKey = "YW3q3vdk03DvUTr26uE7jl7GEh8X95OOnV1NTcFydMI"
+let SecretKey = "EV86WtEkGDrrcCzBIUzm9XZC5M-f-FbE14vDnKE2k-U"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_lik"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
+
